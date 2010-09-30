01bc6e5c1764d2befa76d6680a55a4c94dc32740
Tim Wentz <wentzt@gmail.com>
2010-09-30 14:21:34 -0400

    testing db connect

--- test.rb
+++ test.rb
@@ -1,6 +1,13 @@
 # test.rb
 require 'rubygems'
 require 'sinatra'
+require 'sequel'
+
 get '/' do
-  "Welcome to caffine! #{Time.now}"
+  DB = Sequel.connect(ENV['DATABASE_URL'] || 'mysql://caffeineadmin:sleep@caffeine-sleep.c13gxsgstw8a.us-east-1.rds.amazonaws.com/caffeine')
+  
+  ds = DB[:test]
+  ds2 = ds.filter(:id=>1)
+  
+  "Welcome to caffine! #{ds2}"
 end
