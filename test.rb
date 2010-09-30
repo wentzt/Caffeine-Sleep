c4d6b7619b2dccc7536b50b48ac06648a89d659f
Tim Wentz <wentzt@gmail.com>
2010-09-30 14:24:47 -0400

    testing db connect 2

--- test.rb
+++ test.rb
@@ -6,8 +6,8 @@ require 'sequel'
 get '/' do
   DB = Sequel.connect(ENV['DATABASE_URL'] || 'mysql://caffeineadmin:sleep@caffeine-sleep.c13gxsgstw8a.us-east-1.rds.amazonaws.com/caffeine')
   
-  ds = DB[:test]
-  ds2 = ds.filter(:id=>1)
+  #ds = DB[:test]
+  #ds2 = ds.filter(:id=>1)
   
-  "Welcome to caffine! #{ds2}"
+  "Welcome to caffine!"
 end
