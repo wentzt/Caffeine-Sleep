09f1a7e076e276b509be71b0cf1018b706ad672a
Tim Wentz <wentzt@gmail.com>
2010-10-01 11:07:30 -0400

    converted page to haml

--- test.rb
+++ test.rb
@@ -7,7 +7,7 @@ get '/' do
   DB = Sequel.connect(ENV['DATABASE_URL'] || 'mysql://caffeineadmin:sleep@caffeine-sleep.c13gxsgstw8a.us-east-1.rds.amazonaws.com/caffeine')
   
   ds = DB[:Test]
-  ds2 = ds.first(:id => 2)
+  @ds2 = ds.first(:id => 2)
   
-  "Welcome to caffine! #{ds2[:label]}"
+  haml :index
 end
