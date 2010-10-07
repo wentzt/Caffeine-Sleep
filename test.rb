f32371047fb7bf1bfc14d2ab131a043a2173a69a
Tim Wentz <wentzt@gmail.com>
2010-10-07 14:04:42 -0400

    Converted to use Datamapper, added form to modify DB

--- test.rb
+++ test.rb
@@ -1,14 +1,29 @@
 # test.rb
 require 'rubygems'
 require 'sinatra'
-require 'sequel'
+require 'dm-core'
+require 'haml'
+
+DataMapper.setup(:default, ENV['DB_URL'])
 
 get '/' do
-  
-  DB = Sequel.connect(ENV['DB_URL'])
-  
-  ds = DB[:users]
-  @ds2 = ds.first(:user_id => 1)
+  @user = User.get(2)
   
   haml :index
+end
+
+post '/update' do
+  user = User.get(2)
+  user.fname = params[:fname]
+  user.save
+  
+  redirect('/')
+end
+
+class User
+  include DataMapper::Resource
+
+  property :user_id,        Serial
+  property :fname,          String
+  property :lname,          String
 end
