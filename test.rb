60be6776efcaa211c188c476b20c9a8cb1944dd5
Tim Wentz <wentzt@gmail.com>
2010-10-07 00:01:17 -0400

    removed DB conn string

--- test.rb
+++ test.rb
@@ -5,7 +5,7 @@ require 'sequel'
 
 get '/' do
   
-  DB = Sequel.connect('mysql://caffeine_sleep:sleep@www.wentzt.net/caffeine_sleep')
+  DB = Sequel.connect(ENV['DB_URL'])
   
   ds = DB[:users]
   @ds2 = ds.first(:user_id => 1)
